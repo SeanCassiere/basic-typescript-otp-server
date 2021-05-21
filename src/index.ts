a5589975dc7a38b031b542070bacb3f0b397b96c
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2021-05-21 13:20:02 +0530

    Initial Commit

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import express from "express";
+import dotenv from "dotenv";
+
+const app = express();
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started and running on port: ${PORT}`));
